7c618ab181faf5e98dc4a856c28a6739287bf5bf
Lukas Stevens <mail@lukas-stevens.de>
2020-11-12 13:34:15 +0100

    Add H2.1 to template

--- src/Exercise02.hs
+++ src/Exercise02.hs
@@ -1,1 +1,9 @@
 module Exercise02 where
+
+import Data.List
+
+twoThirdsAverageWinners :: [(String, Int)] -> [String]
+twoThirdsAverageWinners = undefined
+
+lowestUniqueBidder :: [(String, Int)] -> String
+lowestUniqueBidder = undefined
